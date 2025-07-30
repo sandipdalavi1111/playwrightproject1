8a257e07b8efa3003e18fd2838671cd3c4ebb6f3
sandipdalavi1111 <162698827+sandipdalavi1111@users.noreply.github.com>
2025-07-30 12:46:11 +0530

    remove only keyword

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 
 import { test, expect } from '@playwright/test';
 
-test.only('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
